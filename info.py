9d1ca953ca2e3c91e91ed1393f68b4447570c9c2
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2026-07-30 18:23:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,9 +67,9 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://opp:opp@cluster0.xt7unlb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kvijaylookhouse_db_user:kvijaylookhouse_db_user@utsavtvbot.3t8yyfu.mongodb.net/?appName=utsavtvbot")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://trex:trex@cluster0.wbyvtcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "utsavtvbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Lucy_files')
 
 # ============================
