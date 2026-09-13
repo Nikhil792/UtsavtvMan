72559916d543216fb016582a29de4d9726bfadec
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2026-09-13 22:33:52 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -45,7 +45,7 @@ async def save_group(bot, message):
             return
         buttons = [[
                     InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/TrexSupportchatBot'),
-                    InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇꜱ', url='https://t.me/+OXOBPSHFpQhjZWNl')
+                    InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇꜱ', url='https://t.me/+fDuTBBYVwXw2Mjdl')
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -65,7 +65,7 @@ async def save_group(bot, message):
                                                  caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                           InlineKeyboardButton('• ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇs •', url='https://t.me/+OXOBPSHFpQhjZWNl')
+                                                                           InlineKeyboardButton('• ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇs •', url='https://t.me/+fDuTBBYVwXw2Mjdl')
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
